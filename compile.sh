dcc41dacc6e38eba470dd391dc4c9730b1a90189
Gofer (@gofer_ex) <gofer@ex-studio.ingo>
2019-08-02 00:00:39 +0900

    change directory structure

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ percent_encoding_encode.cpp percent_encoding_decode.cpp percent_encoding_test.cpp -I. -lstdc++ -std=c++11 -o test.out
+g++ src/percent_encoding_encode.cpp src/percent_encoding_decode.cpp test/percent_encoding_test.cpp -Iinclude -lstdc++ -std=c++11 -o test.out
