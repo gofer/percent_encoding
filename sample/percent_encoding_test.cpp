2c5dcb6d01baeb0f58a161cecdd43964810ec26b
Gofer (@gofer_ex) <gofer@ex-studio.ingo>
2019-08-03 02:55:54 +0900

    modify tab style

--- sample/percent_encoding_test.cpp
+++ sample/percent_encoding_test.cpp
@@ -4,23 +4,23 @@
 
 void usage(const char *argv0) {
 	std::cerr << "[Usage]" << std::endl;
-  std::cerr << "  " << argv0 << " [message]   : encoding [message]" << std::endl;
-  std::cerr << "  " << argv0 << " -s          : input line from stdin" << std::endl;
+	std::cerr << "  " << argv0 << " [message]    : encoding [message]" << std::endl;
+	std::cerr << "  " << argv0 << " -s           : input line from stdin" << std::endl;
 }
 
 int main(int argc, char **argv) {
 	if (argc <= 1) {
-    usage(argv[0]);
-    return 0;
-  }
+		usage(argv[0]);
+		return 0;
+	}
 
-  std::string src = "";
-  if (std::string(argv[1]) == "-s") {
-    std::cin >> src;
-  } else {
-    src = std::string(argv[1]);
-  }
-  std::cout << PercentEncoding::encode(src) << std::endl;
+	std::string src = "";
+	if (std::string(argv[1]) == "-s") {
+		std::cin >> src;
+	} else {
+		src = std::string(argv[1]);
+	}
+	std::cout << PercentEncoding::encode(src) << std::endl;
 
 	return 0;
 }
